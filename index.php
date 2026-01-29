bff4ac2c768f7329599709368fef48a2f17fd872
AlandeJesus21 <pereztrujilloalandejesus21@gmail.com>
2026-01-29 13:33:01 -0600

    Se generó la página de inicio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Bienvenidos!!!</h1>
+    <h2>Equipo DevSquad</h2>
+    <h3>Integrantes</h3>
+    <lu>
+        <li>
+            <a href="./damacedi.docx">Damacedi López Gómez</a>
+        </li>
+        <li>
+            <a href="./rosario.docx">
+                Rosario G. López
+            </a>
+        </li>
+        <li>
+            <a href="./alan.docx">Alan de Jesús</a>
+        </li>
+        <li>
+            <a href="./eduardo.docx">Eduardo Pinto</a>
+        </li>
+
+    </lu>
+</body>
+
+</html>
