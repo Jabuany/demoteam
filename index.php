eae18497c6e04c1c61d67543865f91670b3d6e35
AlandeJesus21 <pereztrujilloalandejesus21@gmail.com>
2026-01-30 12:12:37 -0600

    cambio en colores de la página

--- index.php
+++ index.php
@@ -5,29 +5,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+    body {
+        background: #f0f0f0;
+    }
+
+    .container {
+        font-size: large;
+        background: white;
+        width: 30%;
+        margin: auto;
+        text-align: center;
+        padding: 5px;
+        box-shadow: none 0 50px rgba(0, 0, 0, 1);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 40px;
+
+    }
+
+    li {
+        display: block;
+    }
+
+    .textoplano {
+        color: black;
+        font-family: Arial, Helvetica, sans-serif;
+
+    }
+    </style>
 </head>
 
 <body>
-    <h1>Bienvenidos!!!</h1>
-    <h2>Equipo DevSquad</h2>
-    <h3>Integrantes</h3>
-    <lu>
-        <li>
-            <a href="./damacedi.docx">Damacedi López Gómez</a>
-        </li>
-        <li>
-            <a href="./rosario.docx">
-                Rosario G. López
-            </a>
-        </li>
-        <li>
-            <a href="./alan.docx">Alan de Jesús</a>
-        </li>
-        <li>
-            <a href="./eduardo.docx">Eduardo Pinto</a>
-        </li>
+    <div class="container">
+        <h1 class="textoplano">Bienvenidos!!!</h1>
+        <h2 class="textoplano1">Equipo DevSquad</h2>
+        <h3 class="textoplano">Integrantes</h3>
+        <lu>
+            <li>
+                <a href="./damacedi.docx">Damacedi López Gómez</a>
+            </li>
+            <li>
+                <a href="./rosario.docx">
+                    Rosario G. López
+                </a>
+            </li>
+            <li>
+                <a href="./alan.docx">Alan de Jesús</a>
+            </li>
+            <li>
+                <a href="./eduardo.docx">Eduardo Pinto</a>
+            </li>
 
-    </lu>
+        </lu>
+    </div>
 </body>
 
 </html>
